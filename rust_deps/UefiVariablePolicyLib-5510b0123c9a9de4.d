8a938160abe796d1346f83d384e03b5f7639fdae
Bret Barkelew <brbarkel@microsoft.com>
2019-08-26 16:31:10 -0700

    Revert "Start moving in new direction for build tools for rapid integration."
    This reverts commit 5f71401a8a980455d30bd59ff770d2a9789617a1.

---
+++ rust_deps/UefiVariablePolicyLib-5510b0123c9a9de4.d
@@ -0,0 +1,7 @@
+C:\_uefi\IntegratedUnittest\mu_basecore\MdeModulePkg\Library\UefiVariablePolicyLibRust\target\debug\deps\UefiVariablePolicyLib-5510b0123c9a9de4.rmeta: UefiVariablePolicyLib.rs
+
+C:\_uefi\IntegratedUnittest\mu_basecore\MdeModulePkg\Library\UefiVariablePolicyLibRust\target\debug\deps\libUefiVariablePolicyLib-5510b0123c9a9de4.rlib: UefiVariablePolicyLib.rs
+
+C:\_uefi\IntegratedUnittest\mu_basecore\MdeModulePkg\Library\UefiVariablePolicyLibRust\target\debug\deps\UefiVariablePolicyLib-5510b0123c9a9de4.d: UefiVariablePolicyLib.rs
+
+UefiVariablePolicyLib.rs:
